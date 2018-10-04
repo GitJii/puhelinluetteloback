8e565c76c1bc19a0256d1de524a1799e953bf3a3
Jaakko <jaska919@hotmail.com>
2018-10-04 20:36:54 +0300

    Alkuperainen versio tehtaviin 1 ja 2

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+
+let phonenumbers = [
+    {
+        name: 'Tero Pitkämäki',
+        number: '040-2349873249',
+        id: 1
+    },
+    {
+        name: 'Matti Muukalainen',
+        number: '040-2340983411',
+        id: 2
+    },
+    {
+        name:'Anselmi Ansalainen',
+        number: '040-3204987324987',
+        id: 3
+    },
+    {
+        name: 'Leonhard Euler',
+        number: '040-31415312',
+        id: 4
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1> Tämä on etusivu! </h1>')
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = phonenumbers.find(person => person.id === id)
+    response.json(person)
+})
+
+app.get('/info', (request,response) => {
+    response.send('<div>puhelinluettelossa on ' + phonenumbers.length + ' henkilön tiedot</div>' 
+    + '<div> <br> </div>' + new Date())
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(phonenumbers)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
